83aa373d98617939944a2185d9d4789033cbd2b3
Yadu Nand B <yadudoc1729@gmail.com>
2019-01-04 17:14:36 -0600

    Removing bad torque template line

--- parsl/providers/torque/template.py
+++ parsl/providers/torque/template.py
@@ -1,14 +1,14 @@
 template_string = '''#!/bin/bash
 
+${scheduler_options}
 #PBS -S /bin/bash
 #PBS -N ${jobname}
 #PBS -m n
-#PBS -k eo
 #PBS -l walltime=$walltime
 #PBS -l nodes=${nodes_per_block}:ppn=${tasks_per_node}
 #PBS -o ${submit_script_dir}/${jobname}.submit.stdout
 #PBS -e ${submit_script_dir}/${jobname}.submit.stderr
-${scheduler_options}
+
 
 ${worker_init}
 
